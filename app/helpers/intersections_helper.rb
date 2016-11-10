4b10da644b3ae5504e3e08eb7dc7d4d43c7e8e41
Benji <pnhbenji@gmail.com>
2016-11-09 20:19:15 -0600

    Add rest-client to facilitate google geocoding api calls

--- app/helpers/intersections_helper.rb
+++ app/helpers/intersections_helper.rb
@@ -1,4 +1,15 @@
 module IntersectionsHelper
+	require 'rest-client'
 
-	
+	def validate_location(intersection)
+		params = {
+			address: intersection,
+			key: ENV['GMAPS_API_KEY']
+		}
+		url = 'https://maps.googleapis/maps/api/geocode/json'
+		response = RestClient::Request.execute(
+			method: :post,
+			url: url,
+			payload: params)
+	end
 end
