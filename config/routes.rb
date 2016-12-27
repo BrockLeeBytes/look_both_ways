2952a49b29f35d7f7f9d3a5793535d502a9189b1
Benji <pnhbenji@gmail.com>
2016-12-27 12:54:48 -0600

    Finish login system

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+
   get 'intersections/new'
 
 	root 'static_pages#home'
@@ -10,6 +11,9 @@ Rails.application.routes.draw do
 	get '/states', to: 'states#index'
 	get '/createintersection', to: 'states#new'
 	post '/createintersection', to: 'states#create_intersection'
+	get '/login', to: 'sessions#new'
+	post '/login', to: 'sessions#create'
+	delete '/logout', to: 'sessions#destroy'
 
 	resources :users, :intersections, :states
 end
