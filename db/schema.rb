762e69578a2a1876e36cd0028d78085761edb767
Benji <pnhbenji@gmail.com>
2016-12-28 17:26:51 -0600

    Add review model and controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161221235511) do
+ActiveRecord::Schema.define(version: 20161228232535) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,13 @@ ActiveRecord::Schema.define(version: 20161221235511) do
     t.string   "formatted_address"
     t.integer  "state_id"
     t.index ["state_id"], name: "index_intersections_on_state_id", using: :btree
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.text     "body"
+    t.integer  "rating"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "states", force: :cascade do |t|
