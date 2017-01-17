68c67843a1083db698751228e04e3b912193f966
Benji <pnhbenji@gmail.com>
2017-01-17 02:23:13 -0600

    Change user pages to show reviews the user has created

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,6 @@
 class ReviewsController < ApplicationController
+	before_action :logged_in_user, only: [:create, :destroy]
+	before_action :correct_user, only: :destroy
 
 	def create
 		@review = current_user.reviews.build(review_params)
@@ -11,11 +13,19 @@ class ReviewsController < ApplicationController
 	end
 
 	def destroy
+		@review.destroy
+		flash[:success] = "Review deleted"
+		redirect_to root_url
 	end
 
 	private
 
 		def review_params
 			params.require(:review).permit(:body, :rating, :intersection_id)
+		end
+
+		def correct_user
+			@review = current_user.reviews.find_by(id: params[:id])
+			redirect_to root_url if @review.nil?
 		end
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,7 @@
 class Review < ApplicationRecord
 	belongs_to :user
 	belongs_to :intersection
+	default_scope -> { order(created_at: :desc) }
 	validates :intersection_id, uniqueness: { scope: :user_id,
 															message: "You've reviewed this intersection already!"}
 end
